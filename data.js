40ca4732ee59db3c6dcbd8f33046c516b955febe
Jasper Catthoor <jasper.catthoor@gmail.com>
2025-04-28 20:50:16 +0200

    Update data.js

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ export const options = [
             {name: 'Hilde', eliminated: false}
         ]
     },
-     {
+    {
         week: 2,
         option: [
             {name: 'Sarah', eliminated: false},
@@ -44,6 +44,51 @@ export const options = [
             {name: 'Els', eliminated: false},
             {name: 'Alexy', eliminated: false},
             {name: 'Hilde', eliminated: false}
+        ]
+    },
+    {
+        week: 3,
+        option: [
+            {name: 'Sarah', eliminated: false},
+            {name: 'Pasquino', eliminated: true},
+            {name: 'Nimrod', eliminated: false},
+            {name: 'Michèle', eliminated: false},
+            {name: 'Pedro', eliminated: false},
+            {name: 'Rusiana', eliminated: true},
+            {name: 'Jan', eliminated: true},
+            {name: 'Els', eliminated: true},
+            {name: 'Alexy', eliminated: false},
+            {name: 'Hilde', eliminated: false}
+        ]
+    },
+    {
+        week: 4,
+        option: [
+            {name: 'Sarah', eliminated: false},
+            {name: 'Pasquino', eliminated: true},
+            {name: 'Nimrod', eliminated: false},
+            {name: 'Michèle', eliminated: false},
+            {name: 'Pedro', eliminated: false},
+            {name: 'Rusiana', eliminated: true},
+            {name: 'Jan', eliminated: true},
+            {name: 'Els', eliminated: true},
+            {name: 'Alexy', eliminated: false},
+            {name: 'Hilde', eliminated: true}
+        ]
+    },
+    {
+        week: 5,
+        option: [
+            {name: 'Sarah', eliminated: false},
+            {name: 'Pasquino', eliminated: true},
+            {name: 'Nimrod', eliminated: false},
+            {name: 'Michèle', eliminated: false},
+            {name: 'Pedro', eliminated: true},
+            {name: 'Rusiana', eliminated: true},
+            {name: 'Jan', eliminated: true},
+            {name: 'Els', eliminated: true},
+            {name: 'Alexy', eliminated: false},
+            {name: 'Hilde', eliminated: true}
         ]
     }
 ];
@@ -91,6 +136,51 @@ export const data = [
         'David': 'Michèle',
         'Jurgen': 'Nimrod',
         'Tim D.': 'Hilde',
+        'Bram': 'Michèle',
+        'Franky': 'Michèle'
+    },
+    {
+        'Teun': 'Sarah',
+        'Jeroen': 'Sarah',
+        'Sven': 'Pedro',
+        'Karen': 'Pedro',
+        'Tim A.': 'Pedro',
+        'Annelore': 'Els',
+        'Jasper': 'Els',
+        'Geert': 'Alexy',
+        'David': 'Michèle',
+        'Jurgen': 'Nimrod',
+        'Tim D.': 'Hilde',
+        'Bram': 'Michèle',
+        'Franky': 'Michèle'
+    },
+    {
+        'Teun': 'Sarah',
+        'Jeroen': 'Sarah',
+        'Sven': 'Pedro',
+        'Karen': 'Pedro',
+        'Tim A.': 'Pedro',
+        'Annelore': 'Nimrod',
+        'Jasper': 'Michèle',
+        'Geert': 'Alexy',
+        'David': 'Sarah',
+        'Jurgen': 'Nimrod',
+        'Tim D.': 'Hilde',
+        'Bram': 'Michèle',
+        'Franky': 'Michèle'
+    },
+    {
+        'Teun': 'Sarah',
+        'Jeroen': 'Sarah',
+        'Sven': 'Nimrod',
+        'Karen': 'Michèle',
+        'Tim A.': 'Pedro',
+        'Annelore': 'Nimrod',
+        'Jasper': 'Michèle',
+        'Geert': 'Alexy',
+        'David': 'Sarah',
+        'Jurgen': 'Nimrod',
+        'Tim D.': 'Nimrod',
         'Bram': 'Michèle',
         'Franky': 'Michèle'
     }
